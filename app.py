94752ff14553db37b2ddb0fa56c8de131e90f3d4
Rohit Shende <rohitshende16@gmail.com>
2018-01-17 08:13:31 +0530

    Added the logic for Text to Speech as well as Speech to Text - Created a small game out of it

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
-import pyttsx3;
+import stt
+import tts
 
-engine = pyttsx3.init();
-engine.say("Hi I am Python 3.. Welcome to my speech tutorial");
-engine.runAndWait();
+correctness = 0
+total = 0
+
+tts.say("Hi, I am a timepass bot. Talk to me..")
+
+while True:
+    tts.say("Say Something..")
+    audio = stt.listen()
+    text = stt.recognize(audio)
+    tts.say("You Said, " + text)
+    print("You Said, " + text)
+    tts.say("Did I gave you the right Answer ? Say Yes, if I did, else say No")
+    audio = stt.listen()
+    text = stt.recognize(audio)
+    if text.lower() == "yes":
+        correctness += 1
+    total += 1
+    tts.say("Do You wish to test me more ? Say Yes or No")
+    audio = stt.listen()
+    text = stt.recognize(audio)
+    if text.lower() == "no":
+        tts.say("For your Information I was " + str((correctness * 100) / total) + " percent correct. Bye Bye. See You Again")
+        break
+    else:
+        tts.say("I am Happy that you wish to play with me.")
---
+++ tts.py
@@ -0,0 +1,9 @@
+import pyttsx3;
+
+engine = pyttsx3.init();
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate-70)
+
+def say(msg):
+    engine.say(msg);
+    engine.runAndWait()
